ff973ef9a7fa6e03b043e451ca1410c8e5b0f96e
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-30 12:48:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 //your code here!
+document.addEventListener("DOMContentLoaded", function () {
+  const infiList = document.getElementById("infi-list");
+  const itemsPerPage = 20;
+  let currentPage = 1;
+
+  // Function to add items to the list
+  function addItems() {
+    for (let i = 1; i <= itemsPerPage; i++) {
+      const listItem = document.createElement("li");
+      listItem.textContent = `Item ${i + (currentPage - 1) * itemsPerPage}`;
+      infiList.appendChild(listItem);
+    }
+  }
+
+  // Initial load
+  addItems();
+
+  // Function to check if scroll is at the bottom
+  function isScrollAtBottom() {
+    return infiList.scrollTop + infiList.clientHeight >= infiList.scrollHeight;
+  }
+
+  // Function to handle the scroll event
+  function handleScroll() {
+    if (isScrollAtBottom()) {
+      currentPage++;
+      addItems();
+    }
+  }
+
+  // Add scroll event listener
+  infiList.addEventListener("scroll", handleScroll);
+});
 
